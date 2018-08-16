24fd1a56f92fd56f3426eb9de4bb4100c1eaf80a
Jason Burns <jasonb315@protonmail.com>
2018-08-16 16:35:24 -0700

    reversed_arr

--- challenges/reverse_array/reverse_array.py
+++ challenges/reverse_array/reverse_array.py
@@ -1,14 +1,14 @@
 def reverse_array(arr):
 
-  if type(arr) is not list:
-    raise TypeError('type error, argument is not a list')
+    if type(arr) is not list:
+        raise TypeError('type error, argument is not a list')
 
-  mid = len(arr) // 2
-  p1, p2, = 0, -1
+    mid = len(arr) // 2
+    p1, p2, = 0, -1
 
-  for _ in range(mid):
-    arr[p1], arr[p2] = arr[p2], arr[p1]
-    p1 += 1
-    p2 -= 1
-  
-  return arr
+    for _ in range(mid):
+        arr[p1], arr[p2] = arr[p2], arr[p1]
+        p1 += 1
+        p2 -= 1
+
+    return arr
--- challenges/reverse_array/test_reverse_array.py
+++ challenges/reverse_array/test_reverse_array.py
@@ -1,15 +1,17 @@
-from .reverse_array import reverse_array
+from reverse_array import reverse_array
+import pytest
 
 
 def test_reverse__array_module_exists():
-  assert reverse_array
+    assert reverse_array
 
 
 def test_list_gets_reversed():
-  expected = [1, 2, 3, 4]
-  actual = reversed_array([4, 3, 2, 1])
-  assert expected == actual
+    expected = [1, 2, 3, 4]
+    actual = reverse_array([4, 3, 2, 1])
+    assert expected == actual
+
 
 def test_argument_must_be_valid_list():
-  with pytest.raises(TypeError):
-    reverse_array(1)
+    with pytest.raises(TypeError):
+        reverse_array(1)

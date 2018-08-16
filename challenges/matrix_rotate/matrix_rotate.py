155439449061820abc4fccb7ca9954d92a2136fe
Jason Burns <jasonb315@protonmail.com>
2018-08-16 16:38:25 -0700

    pic

--- challenges/matrix_rotate/matrix_rotate.py
+++ challenges/matrix_rotate/matrix_rotate.py
@@ -6,12 +6,16 @@ def matrix_rotate(m):
 
     # a = list(zip(m))  # [([1, 1, 1],), ([2, 2, 2],), ([3, 3, 3],)]
     # did not mutate...
+
     # b = list(zip(*m))  # [(1, 2, 3), (1, 2, 3), (1, 2, 3)]
     # flipped horizontally.
+
     # c = list(zip(m[::-1]))  # [([3, 3, 3],), ([2, 2, 2],), ([1, 1, 1],)]
     # flipped vertically!
+
     # d = list(zip(*m[::-1]))  # [(3, 2, 1), (3, 2, 1), (3, 2, 1)]
     # theory: flip borh horizontal and vert?
+
     # result, good rotate, but are tuples not lists!
     # d = list(zip(list(*m[::-1]))) # error, too many args for list.
 
@@ -22,6 +26,7 @@ def matrix_rotate(m):
     # make me a list filled with lists, made 1:1 with this list of tuples..
     # [[3, 2, 1], [3, 2, 1], [3, 2, 1]]
     # !!!
+
 
     return([list(elem) for elem in (list(zip(*m[::-1])))])
 

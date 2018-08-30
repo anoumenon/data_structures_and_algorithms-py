3a7b631134135bb2fce841550e301bb1568f292c
Jason Burns <jasonb315@protonmail.com>
2018-08-29 18:45:58 -0700

    narwhalls!

--- challenges/multi_bracket_validation/multi_bracket_validation.py
+++ challenges/multi_bracket_validation/multi_bracket_validation.py
@@ -3,30 +3,32 @@ from .a_stack import Stack
 
 def multi_bracket_validation(input):
 
-    PAIR = {
-        ']': '[',
-        '}': '{',
-        ')': '(',
-    }
+    if input:
+        PAIR = {
+            ']': '[',
+            '}': '{',
+            ')': '(',
+        }
 
-    stack = Stack()
+        stack = Stack()
 
-    i = 0
-    while i < len(input):
-        print(input[i])
-        if input[i] == '{' or input[i] == '[' or input[i] == '(':
-            # import pdb; pdb.set_trace()
-            stack.push(input[i])
-            print(stack._top)
-        elif input[i] == '}' or input[i] == ']' or input[i] == ')':
-            if PAIR[input[i]] == stack._top._data:
-                stack.pop()
-            else:
-                return False
+        i = 0
+        while i < len(input):
+            if input[i] == '{' or input[i] == '[' or input[i] == '(':
+                # import pdb; pdb.set_trace()
+                stack.push(input[i])
+                print(stack._top)
+            elif input[i] == '}' or input[i] == ']' or input[i] == ')':
+                if PAIR[input[i]] == stack._top._data:
+                    stack.pop()
+                else:
+                    return False
 
-        i += 1
+            i += 1
 
-    if stack._length == 0:
-        return True
+        if stack._length == 0:
+            return True
+        else:
+            return False
     else:
         return False
--- challenges/multi_bracket_validation/test_multi_bracket_validation.py
+++ challenges/multi_bracket_validation/test_multi_bracket_validation.py
@@ -9,55 +9,41 @@ def test_multi_bracket_validation_exists():
     assert multi_bracket_validation
 
 
-def test_pair():
+def test_single_pair():
     """
     str
     """
     check = multi_bracket_validation('()')
     assert check is True
 
-# def test_stack_stacks_iterable(triple_stack):
-#     """
-#     assert that the stack can be instantiated with nodes
-#     """
-#     assert len(triple_stack) == 3
+
+def test_dynamic_pair():
+    """
+    assert that the stack can be instantiated with nodes
+    """
+    check = multi_bracket_validation('[({}{})]')
+    assert check is True
 
 
-# def test_stack_push(triple_stack):
-#     """
-#     assert that a node my be removed and returned with pop
-#     """
-#     assert len(triple_stack) == 3
-#     taller = triple_stack.push(4)
-#     assert len(triple_stack) == 4
-#     assert taller
+def test_complex_pair():
+    """
+    assert that the stack can be instantiated with nodes
+    """
+    check = multi_bracket_validation('{[][][{()()()}][({}{})]}')
+    assert check is True
 
 
-# def test_stack_pop(triple_stack):
-#     """
-#     assert that a node my be removed and returned with pop
-#     """
-#     assert len(triple_stack) == 3
-#     pop_node = triple_stack.pop()
-#     assert len(triple_stack) == 2
-#     assert pop_node
+def test_null_in():
+    """
+    assert that the stack can be instantiated with nodes
+    """
+    check = multi_bracket_validation(None)
+    assert check is False
 
 
-# def test_stack_pop_value(triple_stack):
-#     """
-#     assert that the last value pushed is first removed
-#     """
-#     assert len(triple_stack) == 3
-#     pop_node = triple_stack.pop()
-#     assert len(triple_stack) == 2
-#     assert pop_node._data == 3
-
-
-# def test_pop_empty_false(empty_stack):
-#     """
-#     assert that trying to pop and empty stack returns false
-#     """
-#     assert len(empty_stack) == 0
-#     pop_node = empty_stack.pop()
-#     assert pop_node is False
-
+def test_narwhall():
+    """
+    assert that the stack can be instantiated with nodes
+    """
+    check = multi_bracket_validation('narwhall')
+    assert check is True
